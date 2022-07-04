6a7903fff1c6fefc8c2026f0baa74f34f8b5f3a4
mn <mn>
2022-07-04 14:14:22 +0200

    Changeset: 38 Fix

--- eLogHeavyTools/src/Masters/PriceTable/PrcType.cs
+++ eLogHeavyTools/src/Masters/PriceTable/PrcType.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using eProjectWeb.Framework;
+using eProjectWeb.Framework.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,4 +14,15 @@ namespace eLog.HeavyTools.Masters.PriceTable
         Actual = 1,  // Aktuális
         BasisOfAction = 2,  // Akció alapja
     }
+    public class PrcTypeList : EnumListProvider<PrcType>
+    {
+        public static readonly string ID = typeof(PrcTypeList).FullName;
+
+        public static PrcTypeList New(string xml, string name)
+        {
+            var t = (PrcTypeList)ObjectFactory.New(typeof(XmlBaseDefaultListProvider));
+            return t;
+        }
+    }
+
 }
