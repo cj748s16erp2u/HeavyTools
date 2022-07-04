76e642713a83c34bfbf7e850ed153fc042d1bcbd
ta <ta>
2022-07-04 17:53:01 +0200

    Changeset: 40 HTO-4675: Külső fájlok feldolgozása - SQL fix

--- eLogHeavyTools/Sql/update/20220701.HTO-4675.002.sql
+++ eLogHeavyTools/Sql/update/20220701.HTO-4675.002.sql
@@ -1,6 +1,21 @@
-insert into ols_typeline (typegrpid,value,seqno,name,abbr,int1,int2,num1,num2,str1,str2,date1,date2,addusrid,adddate,delstat)
-                  values (8001,60,60,'Bankkártya kivonat file','KKF',null,null,null,null,null,null,null,null,'dev','20130417',0)
+insert ols_typeline (typegrpid, value, seqno, name, abbr, addusrid, adddate, delstat)
+values (8001, 60, 60, 'K&H Bankkártya kivonat file', 'KHK', 'dev', getdate(), 0)
 
-insert into ols_typeline (typegrpid,value,seqno,name,abbr,int1,int2,num1,num2,str1,str2,date1,date2,addusrid,adddate,delstat)
-                  values (8001,80,80,'Importált avizó file','IMP',null,null,null,null,null,null,null,null,'dev',getdate(),0)
+insert ols_typeline (typegrpid, value, seqno, name, abbr, addusrid, adddate, delstat)
+values (8001, 61, 61, 'OTP Bankkártya kivonat file', 'OTPK', 'dev', getdate(), 0)
+
+insert ols_typeline (typegrpid, value, seqno, name, abbr, addusrid, adddate, delstat)
+values (8001, 62, 62, 'GLS avizó file', 'GLS', 'dev', getdate(), 0)
+
+insert ols_typeline (typegrpid, value, seqno, name, abbr, addusrid, adddate, delstat)
+values (8001, 63, 63, 'FoxPost avizó file', 'FOXP', 'dev', getdate(), 0)
+
+insert ols_typeline (typegrpid, value, seqno, name, abbr, addusrid, adddate, delstat)
+values (8001, 64, 64, 'Sprinter avizó file', 'PPP', 'dev', getdate(), 0)
+
+insert ols_typeline (typegrpid, value, seqno, name, abbr, addusrid, adddate, delstat)
+values (8001, 65, 65, 'Hervis avizó file', 'HER', 'dev', getdate(), 0)
+
+insert ols_typeline (typegrpid, value, seqno, name, abbr, addusrid, adddate, delstat)
+values (8001, 66, 66, 'Intersport avizó file', 'INT', 'dev', getdate(), 0)
 go
