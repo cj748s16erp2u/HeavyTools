39bbf689a8ba3e2ed7e2d5a2c6c5104b32298418
mn <mn>
2022-08-08 16:52:30 +0200

    Changeset: 107 Branch comment: [HTO-4737: OSS adóbevallási rendszer] Adatbázis szerkezet

--- eLogHeavyTools/Sql/cr.sql
+++ eLogHeavyTools/Sql/cr.sql
@@ -443,3 +443,24 @@ alter table [olc_sordline] add constraint [fk_olc_sordline_sordlineid] foreign k
 alter table [olc_sordline] add constraint [fk_olc_sordline_addusrid] foreign key ([addusrid]) references [cfw_user] ([usrid])
 go
 
+create table olc_taxtransext (  
+	tteid 	    		int identity(1, 1),
+	ttid	int not null,
+
+	countryid			varchar(12) not null,
+	taxid				varchar(12) not null, 
+	ttefrom				datetime not null,
+	tteto				datetime not null,
+	
+	addusrid			varchar(12),
+	adddate				datetime,
+	delstat				int not null,
+
+	constraint pk_olc_taxtransext primary key (tteid),
+	constraint fk_olc_taxtransext_addusrid foreign key (addusrid) references cfw_user(usrid),
+	constraint fk_olc_taxtransext_ttid foreign key (ttid) references ols_taxtrans(ttid),
+	constraint fk_olc_taxtransext_countryid foreign key (countryid) references ols_country(countryid),
+	constraint fk_olc_taxtransext_taxid foreign key (taxid) references ols_tax(taxid),
+)
+
+go
